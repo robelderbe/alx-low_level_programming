507c90729d22606ff68148a19aa08f97b2b98fb1
robelderbe <robelderbe@gmail.com>
2022-06-02 14:20:31 -0700

    completed the tasks of file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - prints the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non -zero otherwise
+ * */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a pieceof art, \n");
+	return (0);
+}
