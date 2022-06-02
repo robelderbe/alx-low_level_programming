012ca7d01eba6de4416ed2fe456d133eeb31639d
robelderbe <robelderbe@gmail.com>
2022-06-02 16:15:07 -0700

    completed the tasks of file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return: 0 if exited properly
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
@@ -12,6 +12,7 @@ int main(void)
 	long log int b;
 	log int d;
 	float f;
+
         printf("size of a char: %lu bytes(s)\n",(unsigned long) sizeof(c));
 	printf("size of an int: %lu bytes(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
