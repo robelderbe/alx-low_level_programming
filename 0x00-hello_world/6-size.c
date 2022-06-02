59acde0b60fbf7ef93e5e32a211ca1417e1fff46
robelderbe <robelderbe@gmail.com>
2022-06-02 15:53:01 -0700

    completed the tasks of file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 /**
- * main: prints the size of varios types
+ * main:Run main function
+ *
+ *Description: prints the size of varios types
  * Return: 0 if exited properly
  */
 int main(void)
 {
-        printf("size of a char: %d bytes(s)\n", sizeof(char));
-	printf("size of an int: %d bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	int a;
+	char c;
+	long log int b;
+	log int d;
+	float f;
+        printf("size of a char: %d bytes(s)\n",(unsigned long) sizeof(c));
+	printf("size of an int: %d bytes(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
