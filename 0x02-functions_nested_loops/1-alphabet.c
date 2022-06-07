6f7e4653c6e31f70d0f2fbced1790484b17ffc36
robelderbe <robelderbe@gmail.com>
2022-06-07 15:34:58 -0700

    added tasks 1 to project 0x02

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
