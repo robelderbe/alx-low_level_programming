ed15992903d64a2237f9375add9b54d75daf17cd
robelderbe <robelderbe@gmail.com>
2022-06-07 15:58:36 -0700

    added tasks 2 to project 0x02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+nclude "main.h"                                            
+
+/**
+ * main - prints the alphabet x10
+ *
+ * Description: Prints the alphabet with _putchar ten times
+ * Return: void
+ */
+ 
+ void print_alphabet_x10(void)
+{
+	char j;
+	char a;
+
+	for (a =0; a <= 9; a++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		_putchar(j);
+	
+	_putchar('\n');
+	}
+}
