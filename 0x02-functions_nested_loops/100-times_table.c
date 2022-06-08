77e565fa814fc0e8216acf92d4a6a81943546849
robelderbe <robelderbe@gmail.com>
2022-06-08 01:39:00 -0700

    added tasks 12 to project 0x02

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		if (n >= 0 && n <= 15)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
@@ -21,4 +21,30 @@ void print_times_table(int n)
 				if (j > 0)
 				{
 					_putchar(',');
-					
+					_putchar(' ');
+					if (result < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + result);
+					}
+					else if (result >= 10 && result < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (result / 10));
+						_putchar('0' + (result % 10));
+					}
+					else if (result >= 100)
+					{
+						_putchar('0' + (result / 100));
+						_putchar('0' + ((result / 10) % 10));
+						_putchar('0' + (result % 10));
+					}
+				}
+				else
+					_putchar('0' + result);
+			}
+			_putchar('\n');
+		}
+	}
+}
