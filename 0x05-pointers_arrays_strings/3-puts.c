6e3fc39ccea691c1d077e56cb6df1ceaf77249a5
robelderbe <robelderbe@gmail.com>
2022-06-13 12:38:03 -0700

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Print given string
+ * @str: String to print
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
