3055c645769695e04cddb563c2f864586f175c1b
robelderbe <robelderbe@gmail.com>
2022-07-04 18:57:53 -0700

    fixing:adding NULL case

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,17 +3,18 @@
 
 /**
  * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
+ * @d: pointer to struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
